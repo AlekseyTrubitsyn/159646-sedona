ed493f7de21e90ef37e898cb8753d010c76d2a3e
AlekseyTrubitsyn <at.8441451@yandex.ru>
2016-03-06 23:09:35 +0300

    add popup animation

---
+++ js/script.js
@@ -0,0 +1,76 @@
+var bookingPopup = document.querySelector(".booking-popup");
+var bookingFormShowButton = document.querySelector(".search-form-btn");
+
+var bookingFormAdultsCountButtonMinus = document.querySelector("#booking-form-adults-count-minus-button");
+var bookingFormAdultsCountButtonPlus = document.querySelector("#booking-form-adults-count-plus-button");
+var bookingFormAdultsCountInput = document.querySelector("#booking-form-adults-count-input");
+
+var bookingFormKidsCountButtonMinus = document.querySelector("#booking-form-kids-count-minus-button");
+var bookingFormKidsCountButtonPlus = document.querySelector("#booking-form-kids-count-plus-button");
+var bookingFormKidsCountInput = document.querySelector("#booking-form-kids-count-input");
+
+var adultsCount = bookingFormAdultsCountInput.value;
+var adultsMin = Number(bookingFormAdultsCountInput.min);
+var adultsMax = Number(bookingFormAdultsCountInput.max);
+
+var kidsCount = bookingFormKidsCountInput.value;
+var kidsMin = Number(bookingFormKidsCountInput.min);
+var kidsMax = Number(bookingFormKidsCountInput.max);
+
+bookingFormShowButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  bookingPopup.classList.remove("popup-invisible");
+});
+
+bookingFormAdultsCountButtonMinus.addEventListener("click", function(event) {
+  event.preventDefault();
+  adultsCount = Number(bookingFormAdultsCountInput.value);
+
+  if (adultsCount > adultsMin && adultsCount <= adultsMax )
+    bookingFormAdultsCountInput.value = Number(adultsCount) - 1;
+
+  else if ( kidsCount > adultsMin )
+    bookingFormAdultsCountInput.value = adultsMax;
+
+  else
+    bookingFormAdultsCountInput.value = adultsMin;
+});
+
+bookingFormAdultsCountButtonPlus.addEventListener("click", function(event) {
+  event.preventDefault();
+  adultsCount = bookingFormAdultsCountInput.value;
+  if (adultsCount < adultsMax && adultsCount >= adultsMin)
+    bookingFormAdultsCountInput.value = Number(adultsCount) + 1;
+
+  else if ( kidsCount < adultsMin )
+    bookingFormAdultsCountInput.value = adultsMin;
+
+  else
+    bookingFormAdultsCountInput.value = adultsMax;
+});
+
+bookingFormKidsCountButtonMinus.addEventListener("click", function(event) {
+  event.preventDefault();
+  kidsCount = bookingFormKidsCountInput.value;
+  if (kidsCount > kidsMin && kidsCount <= kidsMax)
+    bookingFormKidsCountInput.value = Number(kidsCount) - 1;
+
+  else if ( kidsCount > kidsMax )
+    bookingFormKidsCountInput.value = kidsMax;
+
+  else
+    bookingFormKidsCountInput.value = kidsMin;
+});
+
+bookingFormKidsCountButtonPlus.addEventListener("click", function(event) {
+  event.preventDefault();
+  kidsCount = bookingFormKidsCountInput.value;
+  if (kidsCount < kidsMax && kidsCount >= kidsMin)
+    bookingFormKidsCountInput.value = Number(kidsCount) + 1;
+
+  else if ( kidsCount < kidsMax )
+    bookingFormKidsCountInput.value = kidsMin;
+
+  else
+    bookingFormKidsCountInput.value = kidsMax;
+});
